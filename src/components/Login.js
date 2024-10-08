09ee0fbc070516352d58eee3d61ab35945bf4089
Roshanshrivas <roshanshrivas11@gmail.com>
2024-10-08 13:13:07 +0530

    signin/sign up form && Login?Header

---
+++ src/components/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import Header from './Header'
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  }
+  return (
+    <div>
+        <Header />
+        <div className='absolute'>
+          <img 
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg"
+          alt='logo'/>
+        </div>
+        <form className='absolute p-12 bg-black w-3/12 mx-auto my-36 right-0 left-0 text-white rounded-lg bg-opacity-80'>
+            <h1 className='font-bold text-3xl py-4'>
+              {
+                isSignInForm ? "Sign In" : "Sign Up"
+              }
+            </h1>
+            {!isSignInForm && ( 
+              <input 
+               type="text" 
+               placeholder='Full Name' 
+               className='p-2 my-2 w-full bg-gray-700'/>
+              )}
+            
+               
+            <input 
+               type="text" 
+               placeholder='Email Address' 
+               className='p-2 my-2 w-full bg-gray-700'/>
+
+            <input 
+              type="text" 
+              placeholder='Password' 
+              className='p-2 my-2 w-full bg-gray-700'/>
+
+            <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+              {isSignInForm ? "Sign In" : "Sign Up"}
+            </button>
+            <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>
+              {
+                isSignInForm ? 
+                "New to Netflix? Sign Up Now" 
+                :
+                "Already Registered? Sign In Now"
+              }
+              </p>
+        </form>
+    </div>
+  )
+}
+
+export default Login
